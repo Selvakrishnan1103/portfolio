ca6f0c708752d827063622579d163615de7759e0
Selva_Krishnan <120071829+Selvakrishnan1103@users.noreply.github.com>
2024-07-28 19:10:53 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,3 +6,23 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.classList.toggle('active');
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggleButton = document.getElementById('modeToggle');
+    const body = document.body;
+
+    // Check for saved user preference and apply theme
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        body.classList.add(savedTheme);
+    }
+
+    // Toggle theme on button click
+    themeToggleButton.addEventListener('click', function() {
+        body.classList.toggle('light-theme');
+        body.classList.toggle('dark-theme');
+
+        // Save user preference to local storage
+        const currentTheme = body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+        localStorage.setItem('theme', currentTheme);
+    });
+});
